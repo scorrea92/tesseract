3568d117dd1edcf3aab88b0ff9246f32c36d4484
Sebastian Correa Echeverri <scorrea92@gmail.com>
2018-09-28 12:07:20 +0200

    Update recodebeam.cpp

--- src/lstm/recodebeam.cpp
+++ src/lstm/recodebeam.cpp
@@ -379,8 +379,8 @@ void RecodeBeamSearch::ExtractPathAsUnicharIds(
   while (t < width) {
     double certainty = 0.0;
     double rating = 0.0;
-    std::cout << "Chart ID: "<< unicharset->id_to_unichar_ext( best_nodes[t]->unichar_id ) << " Certainty: " << best_nodes[t]->certainty << std::endl;
     while (t < width && best_nodes[t]->unichar_id == INVALID_UNICHAR_ID) {
+      std::cout << "Chart ID: "<< unicharset->id_to_unichar_ext( best_nodes[t]->unichar_id ) << " Certainty: " << best_nodes[t]->certainty << std::endl;
       double cert = best_nodes[t++]->certainty;
       if (cert < certainty) certainty = cert;
       rating -= cert;
