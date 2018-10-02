afb35b7a3307ed9456fc671ff45f678ac4dd2266
scorrea <scorrea@das-nano.com>
2018-10-02 14:35:05 +0200

    test white list in ccutil.h

--- src/ccutil/ccutil.h
+++ src/ccutil/ccutil.h
@@ -66,6 +66,7 @@ class CCUtil {
   STRING lang;
   STRING language_data_path_prefix;
   UNICHARSET unicharset;
+  unicharset->set_black_and_whitelist("", "abcd", "");
   UnicharAmbigs unichar_ambigs;
   STRING imagefile;  // image file name
   STRING directory;  // main directory
@@ -73,8 +74,8 @@ class CCUtil {
  private:
   ParamsVectors params_;
 
-  UNICHARSET* charset_1234_s4c0 = new UNICHARSET(unicharset);
-  charset_1234_s4c0->set_black_and_whitelist("", "abcd", "");
+  // UNICHARSET* charset_1234_s4c0 = new UNICHARSET(unicharset);
+  // charset_1234_s4c0->set_black_and_whitelist("", "abcd", "");
 
  public:
   // Member parameters.
