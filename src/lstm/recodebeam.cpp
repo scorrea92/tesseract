b5e25619c5bc63bc67e3d6c3309257f9556a3a1e
scorrea <scorrea@das-nano.com>
2018-10-03 09:44:38 +0200

    test put 0 prob using white list

--- src/lstm/recodebeam.cpp
+++ src/lstm/recodebeam.cpp
@@ -95,7 +95,7 @@ void RecodeBeamSearch::Decode(const NetworkIO& output, double dict_ratio,
     for (int i = 0; i < output_sebas.NumFeatures(); ++i) {
         if (i > 0) {
           if( !charset->get_enabled(i+2)){
-             output_sebas.f(t)[i] = 0.0f;
+             output_sebas.f(t)[i+2] = 0.0f;
           }
         } else {
          if( !charset->get_enabled(i)){
