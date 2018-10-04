ce0c6e666f1925b2dd971f942d1c0bc7b8528f53
scorrea <scorrea@das-nano.com>
2018-10-04 11:02:31 +0200

    Test reomving char 58 in extract best path id

--- src/lstm/recodebeam.cpp
+++ src/lstm/recodebeam.cpp
@@ -415,8 +415,7 @@ void RecodeBeamSearch::DebugBeamPos(const UNICHARSET& unicharset,
 /* static */
 void RecodeBeamSearch::ExtractPathAsUnicharIds(
     const GenericVector<const RecodeNode*>& best_nodes,
-    GenericVector<int>* unichar_ids, GenericVector<float>* 
-    ,
+    GenericVector<int>* unichar_ids, GenericVector<float>* certs,
     GenericVector<float>* ratings, GenericVector<int>* xcoords,
     std::deque<std::pair<int,int>>* best_glyphs) {
   unichar_ids->truncate(0);
@@ -434,10 +433,10 @@ void RecodeBeamSearch::ExtractPathAsUnicharIds(
       if (cert < certainty) certainty = cert;
       rating -= cert;
     }
-    if (t < width) {
+    if (t < width && best_nodes[t]->unichar_id != 58) {
       int unichar_id = best_nodes[t]->unichar_id;
       if (unichar_id == UNICHAR_SPACE && !certs->empty() &&
-          best_nodes[t]->permuter != NO_PERM && unichar_id == 58) {
+          best_nodes[t]->permuter != NO_PERM) {
         // All the rating and certainty go on the previous character except
         // for the space itself.
         if (certainty < certs->back()) certs->back() = certainty;
